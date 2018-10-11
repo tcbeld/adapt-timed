c09c3a13eddead6f0accdf503848d78cdd679d2d
Tony Beld <tonybeld@Tonys-MacBook-Pro-2.local>
2018-10-10 21:09:07 -0600

    initial plugin

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# adapt-timed
+# adapt-timed  
+
+**Timed** is a *presentation component* for the [Adapt framework](https://github.com/adaptlearning/adapt_framework).  
+
+Its purpose is to allow content authors to set a delay before a next button appears to the learner.
+## Installation
+## Limitations
+
+No known limitations.   
+
+----------------------------
+**Version number:**  2.0.1   <a href="https://community.adaptlearning.org/" target="_blank"><img src="https://github.com/adaptlearning/documentation/blob/master/04_wiki_assets/plug-ins/images/adapt-logo-mrgn-lft.jpg" alt="adapt learning logo" align="right"></a> 
+**Framework versions:** 2+  
+**Author / maintainer:** Adapt Core Team with [contributors](https://github.com/adaptlearning/adapt-contrib-blank/graphs/contributors)  
+**Accessibility support:** n/a   
+**RTL support:** n/a  
+**Cross-platform coverage:** Chrome, Chrome for Android, Firefox (ESR + latest version), Edge 12, IE 11, IE10, IE9, IE8, IE Mobile 11, Safari iOS 9+10, Safari OS X 9+10, Opera    
---
+++ js/adapt-timed.js
@@ -0,0 +1,100 @@
+define([
+    "core/js/adapt",
+    "core/js/views/componentView",
+    "core/js/models/componentModel"
+], function(Adapt, ComponentView, ComponentModel){
+
+    var Blank = ComponentView.extend({
+    events: {
+            'click .waitButton': 'viewNext'
+        },
+	viewNext: function() {
+		var gotoBlockID = this.model.get('_gotoBlockID');
+		// Syntax: Adapt.navigateToElement(element, options);
+		Adapt.navigateToElement(gotoBlockID, {duration: 500});
+                
+	},
+        preRender: function() {
+            this.$el.addClass("no-state");
+            // Checks to see if the blank should be reset on revisit
+            this.checkIfResetOnRevisit();
+            this.listenTo(Adapt, 'device:changed', this.resizeImage);
+
+            // Checks to see if the graphic should be reset on revisit
+            this.checkIfResetOnRevisit();
+        },
+
+        postRender: function() {
+	    var waitTime = this.model.get('_waitTime');
+            this.setReadyStatus();
+            this.$('.component-inner').on('inview', _.bind(this.inview, this));
+	    this.$('.component-inner'), this.$('#timedbutton').delay(waitTime).fadeIn(2200);
+	    this.resizeImage(Adapt.device.screenSize, true);
+        },
+        
+	// Used to check if the blank should reset on revisit
+        checkIfResetOnRevisit: function() {
+            var isResetOnRevisit = this.model.get('_isResetOnRevisit');
+
+            // If reset is enabled set defaults
+            if (isResetOnRevisit) {
+                this.model.reset(isResetOnRevisit);
+            }
+        },
+	        /**
+         * determines which element should be used for inview logic - body, instruction or title - and returns the selector for that element
+         */
+        getInviewElementSelector: function() {
+            if(this.model.get('body')) return '.component-body';
+
+            if(this.model.get('instruction')) return '.component-instruction';
+
+            if(this.model.get('displayTitle')) return '.component-title';
+
+            return null;
+        },
+        inview: function(event, visible, visiblePartX, visiblePartY) {
+            if (visible) {
+                if (visiblePartY === 'top') {
+                    this._isVisibleTop = true;
+                } else if (visiblePartY === 'bottom') {
+                    this._isVisibleBottom = true;
+                } else {
+                    this._isVisibleTop = true;
+                    this._isVisibleBottom = true;
+                }
+
+                if (this._isVisibleTop && this._isVisibleBottom) {
+                    this.$('.component-inner').off('inview');
+                    this.setCompletionStatus();
+                }
+            }
+        },
+	            resizeImage: function(width, setupInView) {
+            var imageWidth = width === 'medium' ? 'small' : width;
+            var imageSrc = (this.model.get('_graphic')) ? this.model.get('_graphic')[imageWidth] : '';
+            this.$('.graphic-widget img').attr('src', imageSrc);
+
+            this.$('.graphic-widget').imageready(_.bind(function() {
+                this.setReadyStatus();
+
+                if (setupInView) {
+                    // Bind 'inview' once the image is ready.
+                    this.$('.component-widget').on('inview', _.bind(this.inview, this));
+                }
+            }, this));
+        },
+	    remove: function() {
+            if(this.model.has('inviewElementSelector')) {
+                this.$(this.model.get('inviewElementSelector')).off('inview');
+            }
+
+            ComponentView.prototype.remove.call(this);
+	    },
+  
+        template: 'blank'
+
+    });
+    Adapt.register('blank', Blank);
+    return Blank;
+});
